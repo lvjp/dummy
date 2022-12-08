2d9c0daf0caa1f8f689fdc1ec3b77787f222d136
Laurent VERDOÏA <verdoialaurent@gmail.com>
2022-12-08 23:16:35 +0100

    [AUTH] Run a light HTTP server

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"time"
+
 	"github.com/spf13/cobra"
 	"golang.org/x/exp/slog"
 )
@@ -16,7 +22,36 @@ var authServeCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Run the authentication server",
 	Run: func(cmd *cobra.Command, args []string) {
-		slog.Info("auth serve called")
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			data := bytes.NewBufferString("Hello from the dummy authentication server")
+			length := data.Len()
+
+			w.Header().Set("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusOK)
+
+			written, err := io.Copy(w, data)
+			if err != nil {
+				slog.Error("Cannot write HTTP response", err,
+					slog.Int64("written", written),
+					slog.Int("length", length),
+				)
+			}
+		})
+
+		s := &http.Server{
+			Addr:         ":8080",
+			Handler:      mux,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("Cannot listen and serve", err)
+			return
+		}
+
+		slog.Info("Server gracefully shutdowned")
 	},
 }
 
