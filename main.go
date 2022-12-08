79048265284f1792f1be9934d870c596d8f3e13b
Laurent VERDOÏA <verdoialaurent@gmail.com>
2022-12-08 21:39:07 +0100

    Go with JSON logging

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
 func main() {
-	println("Dummy app...")
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout)))
+	slog.Info("Dummy app...")
 }
