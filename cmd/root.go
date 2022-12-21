03fde17e81ee43f97da27819346e6115b9c1985e
Laurent VERDOÏA <verdoialaurent@gmail.com>
2022-12-21 22:14:06 +0100

    Init the dummy command line interface

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slog"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "dummy",
+	Short: "A simple dummy app",
+	Run: func(cmd *cobra.Command, args []string) {
+		slog.Info("dummy !!!")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		slog.Error("Execution failed", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"os"
 
+	"github.com/lvjp/dummy/cmd"
 	"golang.org/x/exp/slog"
 )
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout)))
-	slog.Info("Dummy app...")
+
+	cmd.Execute()
 }
