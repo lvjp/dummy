138fdcde0d08752e41d587689bf31ab58a656471
Laurent VERDOÏA <verdoialaurent@gmail.com>
2022-12-21 22:30:06 +0100

    Add --version option

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,14 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+// version variable is here to be modified at compile time.
+var version = "(development)"
+
+func init() {
+	rootCmd.Version = version
+	rootCmd.SetVersionTemplate("{{ .Version }}\n")
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
