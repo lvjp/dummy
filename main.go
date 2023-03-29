f316a217221f6cea801609316a82576bad909ef9
Laurent VERDOÏA <verdoialaurent@gmail.com>
2023-03-30 00:02:33 +0200

    Little refactoring of the main

--- main.go
+++ main.go
@@ -22,48 +22,7 @@ func main() {
 	mainCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("Incoming request",
-			slog.Time("time", time.Now()),
-			slog.String("host", r.Host),
-			slog.String("remote", r.RemoteAddr),
-			slog.String("proto", r.Proto),
-			slog.String("URI", r.RequestURI),
-			slog.String("Referer", r.Header.Get("Referer")),
-			slog.String("User-Agent", r.Header.Get("User-Agent")),
-		)
-
-		data := bytes.NewBufferString("Hello from the dummy authentication server")
-		length := data.Len()
-
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusOK)
-
-		written, err := io.Copy(w, data)
-		if err != nil {
-			slog.Error("Cannot write HTTP response", err,
-				slog.Int64("written", written),
-				slog.Int("length", length),
-			)
-		}
-	})
-
-	// PORT is definied by Scaleway serverless containers
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	s := &http.Server{
-		Addr:         ":" + port,
-		Handler:      mux,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		BaseContext: func(_ net.Listener) context.Context {
-			return mainCtx
-		},
-	}
+	s := newServer()
 
 	p := pool.New().WithErrors().WithContext(mainCtx)
 	p.Go(func(ctx context.Context) error {
@@ -73,7 +32,7 @@ func main() {
 
 		logger := slog.With(slog.String("module", "authserver"))
 
-		logger.With("port", port).Info("Start HTTP server")
+		logger.With("addr", s.Addr).Info("Start HTTP server")
 
 		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			slog.Error("Cannot server listen and serve", err)
@@ -97,4 +56,46 @@ func main() {
 	}
 
 	slog.Info("Shutdown sequence finished")
+}
+
+func newServer() *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Incoming request",
+			slog.Time("time", time.Now()),
+			slog.String("host", r.Host),
+			slog.String("remote", r.RemoteAddr),
+			slog.String("proto", r.Proto),
+			slog.String("URI", r.RequestURI),
+			slog.String("Referer", r.Header.Get("Referer")),
+			slog.String("User-Agent", r.Header.Get("User-Agent")),
+		)
+
+		data := bytes.NewBufferString("Hello from the dummy server")
+		length := data.Len()
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+
+		written, err := io.Copy(w, data)
+		if err != nil {
+			slog.Error("Cannot write HTTP response", err,
+				slog.Int64("written", written),
+				slog.Int("length", length),
+			)
+		}
+	})
+
+	// PORT is definied by Scaleway serverless containers
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	return &http.Server{
+		Addr:         ":" + port,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
 }
