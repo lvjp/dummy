2cdec8caee8362791505944a99060ad864b6e5b0
Laurent VERDOÏA <verdoialaurent@gmail.com>
2023-03-31 23:10:03 +0200

    Update Terraform scaleway to ~> 2.14.0

--- deployments/terraform/provider.tf
+++ deployments/terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "~> 2.13.0"
+      version = "~> 2.14.0"
     }
   }
   required_version = "~> 1.4.0"
